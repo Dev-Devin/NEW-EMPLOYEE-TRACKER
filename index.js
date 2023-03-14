224ba6fa1cd254109aaa5223d9dd9f20cf3961b5
Devin Polichetti <devinpolichetti@gmail.com>
2023-03-14 13:08:35 -0400

    linking to database and initializing functions

--- index.js
+++ index.js
@@ -2,3 +2,19 @@
 const inquirer = require("inquirer");
 require("console.table");
 const mysql = require("mysql2");
+
+// connecting to the database, the user will enter their username and password to connect
+const db = mysql.createConnection({
+  host: "localhost",
+  // MySQL username here
+  user: "root",
+  // MySQL password here
+  password: "password",
+  database: "employees_db",
+});
+
+db.connect(function (err) {
+  if (err) throw err;
+  // initializing the employeeMenu function after the connection is made
+  employeeMenu();
+});
