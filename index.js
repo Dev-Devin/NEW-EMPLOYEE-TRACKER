637e227267ced1b24426d0c2f853f4ef8f233cef
Devin Polichetti <devinpolichetti@gmail.com>
2023-03-17 10:06:31 -0400

    adding if statements

--- index.js
+++ index.js
@@ -21,21 +21,41 @@ db.connect(function (err) {
 
 //Menu for employee that stores all the options
 const employeeMenu = () => {
-  inquirer.createPromptModule([{
-    type: 'list',
-    name: 'choice',
-    message: 'what do you want to do?',
-    choices: [
-      "View Departments",
-      "View",
-      "View Employees",
-      "Add Department",
-      "Add Role",
-      "Add Employee",
-      "Update Employee Role",
-    ]
-  }])
-    .then(function (data)){
-    
-  }
-}
+  inquirer
+    .createPromptModule([
+      {
+        type: "list",
+        name: "choice",
+        message: "what do you want to do?",
+        choices: [
+          "View Departments",
+          "View",
+          "View Employees",
+          "Add Department",
+          "Add Role",
+          "Add Employee",
+          "Update Employee Role",
+        ],
+      },
+    ])
+    .then(function (data) {
+      if (data.choice === "View All Departments") {
+        viewAllDepartments();
+      } else if (data.choice === "View All Roles") {
+        viewAllRoles();
+      } else if (data.choice === "View All Employees") {
+        viewAllEmployees();
+      } else if (data.choice === "Add a Department") {
+        addDepartment();
+      } else if (data.choice === "Add a Role") {
+        addRole();
+      } else if (data.choice === "Add an Employee") {
+        addEmployee();
+      } else if (data.choice === "Update an Employee Role") {
+        updateEmployeeRole();
+      } else {
+        // exit
+        db.end();
+      }
+    });
+};
